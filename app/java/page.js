f7d6ef419e046f7f186e683c4207a2b8f5ba04bd
andrii-malakhovtsev <malakhovtsevandrii@gmail.com>
2025-07-12 04:45:39 -0400

    Added YT shorts for java creative section

--- app/java/page.js
+++ app/java/page.js
@@ -4,6 +4,26 @@ export default function JavaPage() {
       <h2>Java Edition: Creative Mode</h2>
       <br></br>
       <p><i><b>Coming in the future...</b></i></p>
+      <br></br>
+      <p>Planned to be based on my old city map: </p>
+      <div class="shorts-wrapper">
+      <div class="short-container">
+        <iframe 
+          src="https://www.youtube.com/embed/c3YCBiGu02E" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowFullScreen>
+        </iframe>
+      </div>
+      <div class="short-container">
+        <iframe 
+          src="https://www.youtube.com/embed/Rl4gHAwp--Y" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowFullScreen>
+        </iframe>
+      </div>
+      </div>
+
+      
     </main>
   );
 }
