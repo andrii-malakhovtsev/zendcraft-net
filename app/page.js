3e7ee02b9fea6fadd01e15a27b34b9c90a3239a9
andrii-malakhovtsev <malakhovtsevandrii@gmail.com>
2025-07-13 01:39:48 -0400

    Additional home page information

--- app/page.js
+++ app/page.js
@@ -6,6 +6,9 @@ export default function MainPage() {
       <p>Your friendly Minecraft Bedrock and Java Server</p>
       <br></br>
       <p>This server is <b>ONLY</b> for <b>whitelisted friends</b></p>
+      <br></br>
+      <p>Make sure to check <b>each server</b> we have with <b>buttons under the logo</b></p>
+      <br></br>
       <p>You can get whitelisted and instructions on how to join servers are on Discord</p>
       <br></br>
       <p><strong><a href="https://discord.gg/McyvbNsrED" target="_blank">Discord Server Link</a></strong></p>
